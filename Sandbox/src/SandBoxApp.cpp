5c64ac5c11fd5963ea08ca230f496ccf8bbb7085
baidu <baidu@summer.local>
2023-09-08 14:28:23 +0800

    add layers，some modifing in CMakeLists.txt and SandBoxApp.cpp

--- Sandbox/src/SandBoxApp.cpp
+++ Sandbox/src/SandBoxApp.cpp
@@ -4,10 +4,26 @@
 
 //#include "Test.h"
 #include <Hazel.h>
+
+class ExampleLayer : public Hazel::Layer {
+public:
+    ExampleLayer() : Layer("Example") {
+
+    }
+
+    void OnUpdate() override{
+        HZ_INFO("ExampleLayer::Update");
+    }
+
+    void OnEvent(Hazel::Event& event) override {
+        HZ_TRACE("ExampleLayer::OnEvent {0}", event.ToString());
+    }
+};
+
 class Sandbox : public Hazel::Application {
 public:
     Sandbox(){
-
+        PushOverlay(new ExampleLayer());
     }
 
     ~Sandbox()
