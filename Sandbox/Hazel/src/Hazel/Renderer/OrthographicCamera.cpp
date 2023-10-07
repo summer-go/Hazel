25aba4008fbdae0573bb859fd31964cb27fb6daf
xiatian05 <xiatian05@192.168.0.102>
2023-10-08 02:45:32 +0800

    revert RecalculateViewMatrix

--- Sandbox/Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
+++ Sandbox/Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
@@ -14,11 +14,11 @@ namespace Hazel {
     }
 
     void OrthographicCamera::RecalculateViewMatrix() {
-//        glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) *
-//                glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) *
+                glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1));
 
-        glm::mat4 transform =
-                glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1)) * glm::translate(glm::mat4(1.0f), m_Position);
+//        glm::mat4 transform =
+//                glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0, 0, 1)) * glm::translate(glm::mat4(1.0f), m_Position);
         m_ViewMatrix = glm::inverse(transform);
         m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
     }
