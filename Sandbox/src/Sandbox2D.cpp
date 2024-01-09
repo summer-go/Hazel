331f96fd089da770cb1fde08894b642954974666
xiatian.05 <xiatian.05@bytedance.com>
2024-01-10 00:57:28 +0800

    batch rendering-support rotating

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -36,13 +36,23 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts) {
 
     {
         HZ_PROFILE_SCOPE("Renderer Draw");
+
+        static float rotation = 0.f;
+        rotation += ts * 50.0f;
+
         Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
-//        Hazel::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, glm::radians(-45.0f), {0.8f, 0.2f, 0.3f, 1.0f});
+
+        // 旋转45°
+        Hazel::Renderer2D::DrawQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45, {0.8f, 0.2f, 0.3f, 1.0f});
+
+        // 静态矩形
         Hazel::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
         Hazel::Renderer2D::DrawQuad({0.5f, -0.5f}, {0.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
-        Hazel::Renderer2D::DrawQuad({-5.0f, -5.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
-//        Hazel::Renderer2D::DrawQuad({-0.5f, -0.5f, 0.0f}, {1.0f, 1.f}, m_CheckerboardTexture, 20.0f);
-        Hazel::Renderer2D::DrawQuad({-0.5f, -0.5f, 0.0f}, {1.0f, 1.f}, m_CoverTexture, 1.0f);
+        Hazel::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
+
+        // 持续旋转45°
+        Hazel::Renderer2D::DrawQuad({1.f, 0.5f, 0.1f}, {0.8f, 0.8f}, rotation, m_CoverTexture, 1.0f);
+
         Hazel::Renderer2D::EndScene();
 
     }
